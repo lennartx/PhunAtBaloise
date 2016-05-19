bdd6400576e176cbccdb1267fb03c2433b5a8fb4
Matthias Cullmann <matthias.cullmann@baloise.ch>
2016-05-19 09:21:56 +0200

    hello world - first failing test

---
+++ src/test/java/com/baloise/od/StartMeUp.java
@@ -0,0 +1,16 @@
+package com.baloise.od;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class StartMeUp {
+
+	
+	@Test
+	public void pushToGithub() {
+		
+		fail("Do stuff!");
+	}
+	
+}
